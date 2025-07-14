2b6142afc7ab746ee94460d3d148754c7cfcae7e
charantm <charantm8787@gmail.com>
2025-07-14 21:08:04 +0530

    .

--- app/Oauth2.py
+++ app/Oauth2.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv('ALGORITHM')
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
-# Schema to get token
+# Schema to get token from header
 Oauth2_schema = OAuth2PasswordBearer(tokenUrl='login')
 
 
