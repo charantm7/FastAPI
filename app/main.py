141a9d3d646816a19de540f3e917a0c7b1380585
charantm <charantm8787@gmail.com>
2025-07-17 19:37:25 +0530

    added the alembic migrations

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .settings import engine
 from .routers import user, post, auth, vote
@@ -8,6 +9,16 @@ models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+origin = ['https://www.google.com/']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials= True,
+    allow_methods=["*"],
+    allow_headers =["*"]
+)
 
 # CRUD Operation
 
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,26 +1,30 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from .. import schema, models, utils, Oauth2
+from .. import models, utils, Oauth2
 from ..settings import get_db
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(tags=['Authentication'])
 
+
+
+def user_validation_for_login(username, password, db):
+
+    user = db.query(models.User).filter(models.User.email == username).first()
+
+    if not user or not utils.verify(password, user.password):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid credentials")
+    
+    token = Oauth2.create_access_token({"user_id":user.id})
+
+    return token
+    
+
+
 @router.post('/login')
 def login(credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     
-    user = db.query(models.User).filter(models.User.email == credentials.username).first()
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid credentials")
-
-    if not utils.verify(credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Invalid Credentials')
-    
-    # create jwt access token
-    jwt_access_token = Oauth2.create_access_token({'user_id':user.id})
-
-    #validate token
+    jwt_access_token = user_validation_for_login(credentials.username, credentials.password, db)
 
 
     return {"access_token": jwt_access_token, "token_type": "bearer"}
